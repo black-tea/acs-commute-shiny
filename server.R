20d8abf0dd3a98211395aa72c73358da4db7081c
black-tea <timothy.c.black@gmail.com>
2017-11-12 21:58:31 -0800

    Consolidation Update
    changed data_fetch.R to prep.R w/ save to .rds files. Updated server.R to load .rds files.

--- server.R
+++ server.R
@@ -3,71 +3,14 @@
 #####                                                          #####
 
 ##### Setup #####
-library(acs) # For downloading ACS Data
-library(tigris) # For census boundary data
 library(leaflet) # For interactive mapping
-library(rgdal) # For importing shapefiles
 library(sf) # For manipulating spatial data
-#api.key.install(key="00a2d85a7c7b1346879d0e355cc0361f30188f28") # Personal Email Census API Key
-api.key.install(key="9890281ff4c9bdd8980fc6ee6692b00769853229") # City Email Census API Key
-options(tigris_use_cache = TRUE)
+library(tigris)
 
-##### Download / Prep Data #####
-### LA City Boundary
-la_boundary <- rgdal::readOGR('data/LACityBoundary/CityBoundary.shp')
-la_boundary <- st_as_sf(la_boundary)
-la_boundary <- st_transform(la_boundary, 4326)
-
-### Tigris: ACS Boundaries
-options(tigris_class = "sf")
-tracts <- tracts(state = 'CA', county = "037", cb=TRUE) # If cb is set to TRUE, download a generalized (1:500k) tracts file. Defaults to FALSE (the most detailed TIGER/Line file)
-tracts <- st_transform(tracts, 4326)
-
-### ACS: Means of Transportation for all census tracts in the Los Angeles County
-years <- c(2010,2011,2012,2013,2014,2015)
-la_tracts <- geo.make(state = "CA", county = "Los Angeles", tract = "*")
-
-# Lapply function to download ACS data for selected years
-la_tract_modesplit <- lapply(years, function(x) {
-  
-  print(x)
-  
-  # Use the ACS API to query for the data
-  acs_result <- acs.fetch(endyear = x, 
-                          span = 5,
-                          geography = la_tracts,
-                          table.number = "B08301",
-                          col.names = "pretty")
-  
-  # Convert to data.frame for merging
-  year_df <- data.frame(paste0(str_pad(acs_result@geography$state, 2, "left", pad="0"), 
-                               str_pad(acs_result@geography$county, 3, "left", pad="0"), 
-                               str_pad(acs_result@geography$tract, 6, "left", pad="0")), 
-                        acs_result@estimate[,c(
-                          "Means of Transportation to Work: Total:",
-                          "Means of Transportation to Work: Car, truck, or van:",
-                          "Means of Transportation to Work: Public transportation (excluding taxicab):",
-                          "Means of Transportation to Work: Bicycle",
-                          "Means of Transportation to Work: Walked")], 
-                        stringsAsFactors = FALSE)
-  
-  # Clean & Format df
-  rownames(year_df)<-1:nrow(year_df)
-  names(year_df)<-c("GEOID","total", "car", "transit", "bicycle", "walk")
-  
-  # Calculate & Add Mode Splits
-  year_df$car_pct <- 100*(year_df$car/year_df$total)
-  year_df$transit_pct <- 100*(year_df$transit/year_df$total)
-  year_df$bicycle_pct <- 100*(year_df$bicycle/year_df$total)
-  year_df$walk_pct <- 100*(year_df$walk/year_df$total)
-  
-  # Subset out the perentages, then rename with year
-  pct_names <- c("car_pct", "transit_pct", "bicycle_pct", "walk_pct")
-  year_pct <- data.frame(year_df[,c("GEOID","car_pct", "transit_pct", "bicycle_pct", "walk_pct")])
-  
-  return(year_pct)
-})
-names(la_tract_modesplit) <- years
+### Load data from prep.R
+la_boundary <- readRDS('la_boundary.rds')
+la_tract_modesplit <- readRDS('la_tract_modesplit.rds')
+tracts <- readRDS('la_tracts.rds')
 
 ##### Interactive Map #####
 function(input, output, session) {
